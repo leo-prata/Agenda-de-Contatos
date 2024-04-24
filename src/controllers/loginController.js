d281bc991747b872ca23cdd64c30c411eb53a155
leo-prata <leo.prata@estudante.ufjf.br>
2024-04-24 20:16:56 -0300

    inicio da validacao feito

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,3 +1,12 @@
+const Login = require("../models/LoginModel");
+
 exports.index = (req, res, next) => {
   res.render("login");
+};
+
+exports.register = (req, res) => {
+  const login = new Login(req.body);
+  login.register();
+
+  res.send(login.errors);
 };
