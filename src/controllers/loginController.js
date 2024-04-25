b135f2b3d523af56c608c62c14fc537a831b7732
leo-prata <leo.prata@estudante.ufjf.br>
2024-04-24 21:03:37 -0300

    mensagem de sucesso aparecendo

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -5,16 +5,24 @@ exports.index = (req, res, next) => {
 };
 
 exports.register = async function (req, res) {
-  const login = new Login(req.body);
-  await login.register();
+  try {
+    const login = new Login(req.body);
+    await login.register();
 
-  if (login.errors.length > 0) {
-    req.flash("errors", login.errors);
+    if (login.errors.length > 0) {
+      req.flash("errors", login.errors);
+      req.session.save(function () {
+        return res.redirect("back");
+      });
+      return;
+    }
+
+    req.flash("success", "Seu usuário foi criado com sucesso");
     req.session.save(function () {
       return res.redirect("back");
     });
-    return;
+  } catch (e) {
+    console.log(e);
+    return res.render("404");
   }
-
-  res.send(login.errors);
 };
