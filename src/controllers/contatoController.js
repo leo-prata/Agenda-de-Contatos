ede6c8caa62fadc692553cf7620fcf242e6d3b9c
leo-prata <leo.prata@estudante.ufjf.br>
2024-04-25 19:22:09 -0300

    edicao de contatos

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -43,3 +43,29 @@ exports.editIndex = async (req, res) => {
     contato: contato,
   });
 };
+
+exports.edit = async function (req, res) {
+  try {
+    if (!req.params.id) {
+      return res.render("404");
+    }
+    const contato = new Contato(req.body);
+    await contato.edit(req.params.id);
+
+    if (contato.errors.length > 0) {
+      req.flash("errors", contato.errors);
+      req.session.save(() => {
+        res.redirect("back");
+      });
+      return;
+    }
+
+    req.flash("success", "Contato atualizado!");
+    req.session.save(() => {
+      res.redirect(`/contato/index/${contato.contato._id}`);
+    });
+    return;
+  } catch (e) {
+    res.render("404");
+  }
+};
